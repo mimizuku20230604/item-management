6efae4961e5a2d1b5968ce59a10e8d8e1198546d
mimizuku20230604 <130191718+mimizuku20230604@users.noreply.github.com>
2023-10-19 03:22:35 +0900

    2023.10.19-2（create整え）

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -11,65 +11,58 @@
 
   <div class="row">
     <div class="col-12">
-
       @include('includes.alert')
-
       <div class="card">
         <div class="card-header">
           <form method="get" action="{{route('order.confirm')}}" >
             @csrf
-              <div class="form-group">
-                <input type="hidden" name="price_id" value="{{ $price->id }}">
-                <label for="item_id">商品名</label>
-                <input type="hidden" name="item_id" value="{{ $price->item->id }}">
-                <input type="text" class="form-control" name="item_name" id="item_name" value="{{ $price->item->name }}" readonly>
-              </div>
-              <div class="form-group">
-                  <label for="customer_id">顧客名</label>
-                  <input type="hidden" name="customer_id" value="{{ auth()->user()->id }}">
-                  <input type="text" class="form-control" name="customer_name" id="customer_name" value="{{ auth()->user()->name }}" readonly>
-              </div>
-              <div class="form-row">
-                <div class="col-md-4">
-                  <div class="form-group">
-                    <label for="registration_price">単価</label>
-                    <input type="text" name="registration_price" class="form-control" id="registration_price" value="{{ number_format($price->registration_price, 2) }}" readonly>
-                  </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="form-group">
-                      <label for="quantity">数量</label>
-                      <input type="text" name="quantity" class="form-control" id="quantity" value="{{ !empty($request["quantity"]) ? $request["quantity"] : old('quantity') }}" placeholder="数量を入力してください">
-                  </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="form-group">
-                      <label for="total_amount">合計金額</label>
-                      <input type="text" name="total_amount" class="form-control" id="total_amount" readonly>
-                  </div>
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="col-md-4">
-                  <div class="form-group">
-                    <label for="request_date">希望着日（未入力の場合、最短対応）</label>
-                    <input type="date" name="request_date" class="form-control" id="request_date" value="{{ !empty($request["request_date"]) ? $request["request_date"] : old('request_date', '') }}"> {{-- デフォルト値:null --}}
-                  </div>
-                </div>
+            <div class="form-group">
+              <input type="hidden" name="price_id" value="{{ $price->id }}">
+              <label for="item_id">商品名</label>
+              <input type="hidden" name="item_id" value="{{ $price->item->id }}">
+              <input type="text" class="form-control" name="item_name" id="item_name" value="{{ $price->item->name }}" readonly>
             </div>
-              <div class="form-group">
-                <label for="remarks">備考</label>
-                <textarea name="remarks" class="form-control" id="remarks" cols="30" rows="5">{{ !empty($request["remarks"]) ? $request["remarks"] : old('remarks', $price->remarks) }}</textarea>
+            <div class="form-group">
+              <label for="customer_id">顧客名</label>
+              <input type="hidden" name="customer_id" value="{{ auth()->user()->id }}">
+              <input type="text" class="form-control" name="customer_name" id="customer_name" value="{{ auth()->user()->name }}" readonly>
+            </div>
+            <div class="form-row">
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label for="registration_price">単価</label>
+                  <input type="text" name="registration_price" class="form-control" id="registration_price" value="{{ number_format($price->registration_price, 2) }}" readonly>
+                </div>
               </div>
-
-
-
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label for="quantity">数量</label>
+                  <input type="text" name="quantity" class="form-control" id="quantity" value="{{ !empty($request["quantity"]) ? $request["quantity"] : old('quantity') }}" placeholder="数量を入力してください">
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label for="total_amount">合計金額</label>
+                  <input type="text" name="total_amount" class="form-control" id="total_amount" readonly>
+                </div>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label for="request_date">希望着日（未入力の場合、最短対応）</label>
+                  <input type="date" name="request_date" class="form-control" id="request_date" value="{{ !empty($request["request_date"]) ? $request["request_date"] : old('request_date', '') }}"> {{-- デフォルト値:null --}}
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="remarks">備考</label>
+              <textarea name="remarks" class="form-control" id="remarks" cols="30" rows="5">{{ !empty($request["remarks"]) ? $request["remarks"] : old('remarks', $price->remarks) }}</textarea>
+            </div>
             <button type="submit" class="btn btn-outline-success mt-3">確認</button>
           </form>
           <button class="btn btn-secondary mt-3" onclick="location.href='{{route('order.show', $price)}}';">詳細へ戻る</button><br>
           <button class="btn btn-secondary mt-3" onclick="location.href='{{route('order.index')}}';">一覧へ戻る</button>
-
-
         </div>
       </div>
     </div>
@@ -81,33 +74,33 @@
 @stop
 
 @section('js')
-                                        <script>
-                                            // 各フィールドの入力要素を取得
-                                            const quantityInput = document.getElementById('quantity');
-                                            const PriceInput = document.getElementById('registration_price');
-                                            const totalAmountInput = document.getElementById('total_amount');
+  <script>
+    // 各フィールドの入力要素を取得
+    const quantityInput = document.getElementById('quantity');
+    const PriceInput = document.getElementById('registration_price');
+    const totalAmountInput = document.getElementById('total_amount');
 
-                                            // 数値をフォーマットしてカンマを削除する関数
-                                            function formatAndRemoveCommas(value) {
-                                                // カンマを削除してから数値に変換
-                                                return parseFloat(value.replace(/,/g, ''));
-                                            }
+    // 数値をフォーマットしてカンマを削除する関数
+    function formatAndRemoveCommas(value) {
+      // カンマを削除してから数値に変換
+      return parseFloat(value.replace(/,/g, ''));
+    }
 
-                                            // 合計金額を計算して設定する関数
-                                            function calculateTotalAmount() {
-                                                const quantity = formatAndRemoveCommas(quantityInput.value) || 0;
-                                                const Price = formatAndRemoveCommas(PriceInput.value) || 0;
-                                                let totalAmount = quantity * Price;
-                                                totalAmount = Math.round(totalAmount);
-                                                // 数値を桁区切りスタイルにフォーマットして設定
-                                                totalAmountInput.value = totalAmount.toLocaleString();
-                                            }
+    // 合計金額を計算して設定する関数
+    function calculateTotalAmount() {
+      const quantity = formatAndRemoveCommas(quantityInput.value) || 0;
+      const Price = formatAndRemoveCommas(PriceInput.value) || 0;
+      let totalAmount = quantity * Price;
+      totalAmount = Math.round(totalAmount);
+      // 数値を桁区切りスタイルにフォーマットして設定
+      totalAmountInput.value = totalAmount.toLocaleString();
+    }
 
-                                            // 各フィールドの入力イベントにリスナーを追加
-                                            quantityInput.addEventListener('input', calculateTotalAmount);
-                                            PriceInput.addEventListener('input', calculateTotalAmount);
+    // 各フィールドの入力イベントにリスナーを追加
+    quantityInput.addEventListener('input', calculateTotalAmount);
+    PriceInput.addEventListener('input', calculateTotalAmount);
 
-                                            // 初期状態で合計金額を計算
-                                            calculateTotalAmount();
-                                            </script>
+    // 初期状態で合計金額を計算
+    calculateTotalAmount();
+  </script>
 @stop
